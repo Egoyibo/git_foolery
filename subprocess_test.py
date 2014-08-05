055f1e1672344dbd545a4f6b3e9f2a0db05ea707
Jessica Mong <jmong@surveymonkey.com>
2014-08-04 17:15:29 -0700

    change

--- subprocess_test.py
+++ subprocess_test.py
@@ -3,6 +3,13 @@ from sys import argv
 
 script, branch_1, branch_2 = argv
 
+def git(*args):
+	return subprocess.check_call(['git'] + args)
+
+args = ('diff,--name-status,%s...%s') %(branch_1, branch_2)
+args_split = args.split(',')
+print git(args_split )
+
 # print subprocess.check_output(["echo", "Hello World!!!"])
 
 # print subprocess.check_call(["ls"])
