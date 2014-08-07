57689cdf124ed5ea9569ced6a1f1672931157942
Jessica Mong <jmong@surveymonkey.com>
2014-08-07 15:08:33 -0700

    For realsies, ready!

--- subprocess_test.py
+++ subprocess_test.py
@@ -2,13 +2,3 @@ import subprocess
 from sys import argv
 
 script, branch_1, branch_2 = argv
-
-
-
-
-args = "%s...%s" %(branch_2, branch_1)
-print git("diff", args)
-
-def git(*args):
-	# import pdb; pdb.set_trace()
-	return subprocess.check_output(['git'] + list(args))
