40af1e24e01449d24bc392d2f1fdbce679686c74
Jessica Mong <jmong@surveymonkey.com>
2014-08-07 15:06:31 -0700

    ReadY!

--- subprocess_test.py
+++ subprocess_test.py
@@ -3,18 +3,12 @@ from sys import argv
 
 script, branch_1, branch_2 = argv
 
+
+
+
+args = "%s...%s" %(branch_2, branch_1)
+print git("diff", args)
+
 def git(*args):
 	# import pdb; pdb.set_trace()
 	return subprocess.check_output(['git'] + list(args))
-
-args = "%s...%s" %(branch_1, branch_2)
-print git("diff", args)
-
-# print subprocess.check_output(["echo", "Hello World!!!"])
-
-# print subprocess.check_call(["ls"])
-
-# def git(*args):
-# 	return subprocess.check_call(['git'] + list(args))
-
-# print git('status')
